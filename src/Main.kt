4584e8592bc27d77bb87caa40229ea2a4fc35edf
keithunt-35 <keithunt35@duck.com>
2024-12-10 11:13:54 +0300

    initial commit

---
+++ src/Main.kt
@@ -0,0 +1,7 @@
+fun main(args:Array<String>) {
+    println("Hello World!")
+
+    //try adding progrsmm srgumentd vis run
+    //learn more about running applications:
+    println("programm arguments: ${args.joinToString()}")
+}
